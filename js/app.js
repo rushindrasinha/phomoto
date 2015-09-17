eccde3b48b43d7058382298bdcf1935ed53ff14b
Cindy Tong <tongcindyy@gmail.com>
2015-09-17 12:42:28 -0400

    Fixed autorefresh and add background

--- js/app.js
+++ js/app.js
@@ -1,29 +1,4 @@
 
-  //original coding, pulls instagram data once
-  
-  // $.ajax({
-  //   type: "GET",
-  //   dataType: "jsonp",
-  //   cache: false,
-  //   url: "https://api.instagram.com/v1/media/popular/?access_token=25584698.bf54265.749110f8924a47389ea6e818e4ed0941",
-
-  //   success: function(data) {
-  //     console.log("success");
-  //     console.log(data);
-  //     for (var i = 0; i < 6 ; i++) {
-  //     $(".instapopular").append("<div class='col-md-4'><a href='"+ data.data[i].link +"' target=_blank ><img class='img-responsive img-circle' src='"+ data.data[i].images.low_resolution.url +"'</img></a><hr><a href='https://instagram.com/"+ data.data[i].user.username +"' target=_blank ><img class='img-responsive img-circle profilepicture' src='"+ data.data[i].user.profile_picture +"'></img> <span class='username'><b>Username: </b>" + data.data[i].user.username + "</span></a><hr><span class='glyphicon glyphicon-heart' style='color:red'> "+ data.data[i].likes.count+"</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='glyphicon glyphicon-pencil' style='color:blue'> "+ data.data[i].comments.count +"</span><br><hr></div>")
-  //     }
-  //   }
-  // });
-
-// This reloads the page continuously but does not allow music to play
-// setTimeout(function(){
-//    window.location.reload(1);
-// }, 5000);
-
-
-
-//this continuously adds more photos but does not swap out the old ones
 var callback = function(){
   $.ajax({
     type: "GET",
@@ -34,36 +9,12 @@ var callback = function(){
     success: function(data) {
       console.log("success");
       console.log(data);
+      $(".instapopular").empty();
       for (var i = 0; i < 6 ; i++) {
       $(".instapopular").append("<div class='col-md-4'><a href='"+ data.data[i].link +"' target=_blank ><img class='img-responsive img-circle' src='"+ data.data[i].images.low_resolution.url +"'</img></a><hr><a href='https://instagram.com/"+ data.data[i].user.username +"' target=_blank ><img class='img-responsive img-circle profilepicture' src='"+ data.data[i].user.profile_picture +"'></img> <span class='username'><b>Username: </b>" + data.data[i].user.username + "</span></a><hr><span class='glyphicon glyphicon-heart' style='color:red'> "+ data.data[i].likes.count+"</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='glyphicon glyphicon-pencil' style='color:blue'> "+ data.data[i].comments.count +"</span><br><hr></div>")
       }
     }
   });
-      setTimeout(callback, 1000);
+      setTimeout(callback, 5000);
 };
 callback();
-
-
-//Setinterval method - continuously calls new data and does not allow existing to load
-
-// var callback = function(){
-//   $.ajax({
-//       type: "GET",
-//       dataType: "jsonp",
-//       cache: false,
-//       url: "https://api.instagram.com/v1/media/popular/?access_token=25584698.bf54265.749110f8924a47389ea6e818e4ed0941",
-
-//       success: function(data) {
-//         console.log("success");
-//         console.log(data);
-//         for (var i = 0; i < 6 ; i++) {
-//         $(".instapopular").append("<div class='col-md-4'><a href='"+ data.data[i].link +"' target=_blank ><img class='img-responsive img-circle' src='"+ data.data[i].images.low_resolution.url +"'</img></a><hr><a href='https://instagram.com/"+ data.data[i].user.username +"' target=_blank ><img class='img-responsive img-circle profilepicture' src='"+ data.data[i].user.profile_picture +"'></img> <span class='username'><b>Username: </b>" + data.data[i].user.username + "</span></a><hr><span class='glyphicon glyphicon-heart' style='color:red'> "+ data.data[i].likes.count+"</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='glyphicon glyphicon-pencil' style='color:blue'> "+ data.data[i].comments.count +"</span><br><hr></div>")
-//         }
-//       }
-//     });
-// };
-
-// var SetInterval = function (callback){};
-
-// SetInterval(callback,5000);
-
